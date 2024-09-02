8e6dd0f74f718fd8c4327db7b67e4263325b7a23
João Gabriel <jgab2005@live.com>
2024-09-02 17:25:37 -0300

    Update Diagnoscar.sql

--- Diagnoscar.sql
+++ Diagnoscar.sql
@@ -16,45 +16,47 @@ CREATE TABLE Cliente (
 -- Tabela Automovel
 CREATE TABLE Automovel (
     Placa_Automovel VARCHAR2(7) PRIMARY KEY NOT NULL,
-    Cliente_CPF_Cliente VARCHAR2(14),
     Marca_Automovel VARCHAR2(20) NOT NULL, 
     Modelo_Automovel VARCHAR2(100) NOT NULL,
+    Cor_Automovel VARCHAR2(15) NOT NULL,
     Ano_Automovel NUMBER(4) NOT NULL,
+    Cliente_CPF_Cliente VARCHAR2(14),
     CONSTRAINT Automovel_PK PRIMARY KEY (Placa_Automovel),
     CONSTRAINT chk_marca CHECK (Marca_Automovel IN ('Toyota', 'Honda', 'Ford', 'Chevrolet', 'Nissan', 'Volkswagen', 'Hyundai', 'Kia', 'BMW', 'Mercedes-Benz','Audi', 'Jeep', 'Subaru', 'Mazda', 'Fiat', 'Renault', 'Peugeot', 'Mitsubishi', 'Volvo', 'Land Rover')),
     CONSTRAINT Automovel_Cliente_FK FOREIGN KEY (Cliente_CPF_Cliente) REFERENCES Cliente(CPF_Cliente)
 );
 
--- Tabela Assistente
-CREATE TABLE Assistente (
-    ID_Chatbot VARCHAR2(1000),
+-- Tabela Chatbot
+CREATE TABLE Chatbot(
+    ID_Chatbot VARCHAR2(1000) NOT NULL,
     Horario_Chat_Chatbot DATE NOT NULL,
     Plano_Chatbot VARCHAR2(10) NOT NULL,
     Tipodechat_Chatbot CHAR(1) NOT NULL,
     Cliente_CPF_Cliente VARCHAR2(14),
-    CONSTRAINT Asistente_PK PRIMARY KEY (Cliente_CPF_Cliente, ID_Chatbot),
-    CONSTRAINT Assistente_Cliente_FK FOREIGN KEY (Cliente_CPF_Cliente) REFERENCES Cliente(CPF_Cliente)
+    CONSTRAINT Chatbot_PK PRIMARY KEY (ID_Chatbot, Cliente_CPF_Cliente),
+    CONSTRAINT Chatbot_Cliente_FK FOREIGN KEY (Cliente_CPF_Cliente) REFERENCES Cliente(CPF_Cliente)
 );
 
 -- Tabela Pre_Diagnostico
 CREATE TABLE Pre_Diagnostico (
-    ID_PreDiagnostico NUMBER PRIMARY KEY NOT NULL,
-    Nivel_Diagnostico CHAR(3),
-    Diagnostico VARCHAR2(255),
-    Assistente_ID_Chatbot VARCHAR2(1000),
-    CONSTRAINT FK_Assistente FOREIGN KEY (Assistente_ID_Chatbot) REFERENCES Assistente(ID_Chatbot)
+    ID_PreDiagnostico VARCHAR(1000) NOT NULL,
+    Nivel_Diagnostico NUMBER(3) CHECK (Nivel_Diagnostico >= 1 AND Nivel_Diagnostico <= 100), 
+    Diagnostico VARCHAR2(500) NOT NULL,
+    Assistente_ID_Chatbot VARCHAR2(1000) NOT NULL,
+    CONSTRAINT PreDiagnostico_PK PRIMARY KEY (ID_PreDiagnostico),
+    CONSTRAINT PreDiagnostico_Chatbot_FK FOREIGN KEY (Chatbot_ID_Chatbot) REFERENCES Chatbot(ID_Chatbot)
 );
-
 
 -- Tabela Oficina_Parceira
 CREATE TABLE Oficina_Parceira (
     Endereco_Oficina VARCHAR2(200),
+    Cnpj_Oficina VARCHAR2(18) NOT NULL,
     Nome_Oficina VARCHAR2(120) NOT NULL,
     Avaliacao_Oficina NUMBER(3, 2),
     Especializacao_Oficina VARCHAR2(50) NOT NULL,
-    CONSTRAINT Oficina_PK PRIMARY KEY (Endereco_Oficina)
+    CONSTRAINT Oficina_PK PRIMARY KEY (Endereco_Oficina, Cnpj_Oficina),
+    CONSTRAINT Cnpj_Oficina_Check CHECK (REGEXP_LIKE(Cnpj_Oficina, '^[0-9]{2}\.[0-9]{3}\.[0-9]{3}/[0-9]{4}-[0-9]{2}$'))
 );
-
 
 -- Tabela Peca
 CREATE TABLE Peca (
@@ -80,29 +82,7 @@ CREATE TABLE Vincula (
     PRIMARY KEY (Assistente_CPF_Cliente, Oficina_Parceira_Endereco)
 );
 
--- Inserções para a Tabela Cliente
-INSERT INTO Cliente VALUES ('539.371.598-60', 8672017436, 'João Gabriel', 'Boaventura Marques e Silva', 'joao.silva@gmail.com.br', '(11)99365-5770', 'Rua das Flores, 123, São Paulo, SP');
 
--- Inserções para a Tabela Automovel
-INSERT INTO Automovel VALUES ('ABC1D23', '539.371.598-60', 'Fusca', 1980);
-
--- Inserções para a Tabela Assistente
-INSERT INTO Assistente VALUES ('chat123', SYSDATE, 'Premium', 'A', '539.371.598-60');
-
--- Inserções para a Tabela Oficina_Parceira
-INSERT INTO Oficina_Parceira VALUES ('Rua do Comércio, 456, São Paulo, SP', 'Loja do Mecânico');
-
--- Inserções para a Tabela Pre_Diagnostico
-INSERT INTO Pre_Diagnostico VALUES (1, 'Problema no sistema de freios', 'chat123');
-
--- Inserções para a Tabela Peca
-INSERT INTO Peca VALUES (1, 'Pastilha de Freio', 'Rua do Comércio, 456, São Paulo, SP');
-
--- Inserções para a Tabela Entrega
-INSERT INTO Entrega VALUES (1, 'Rua do Comércio, 456, São Paulo, SP');
-
--- Inserções para a Tabela Vincula
-INSERT INTO Vincula VALUES ('539.371.598-60', 'Rua do Comércio, 456, São Paulo, SP');
 
 
 -- Comandos de DROP TABLE
